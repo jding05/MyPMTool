60e288fd9d66583c52df674388a9baa09a036ded
jding05 <jeffding05@gmail.com>
2019-11-07 16:28:55 -0800

    initial commit

---
+++ server/src/main/java/com/jeffding05/mypmtool/MypmtoolApplication.java
@@ -0,0 +1,13 @@
+package com.jeffding05.mypmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypmtoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MypmtoolApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/com/jeffding05/mypmtool/MypmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeffding05.mypmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypmtoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
