e50be539f497c7383ef048a827e3d9a3af86afd6
jding05 <jeffding05@gmail.com>
2019-11-07 21:06:39 -0800

    create/modifed Project class and create ProjectRespository interface

---
+++ server/src/main/java/com/jeffding05/mypmtool/repositories/ProjectRepository.java
@@ -0,0 +1,12 @@
+package com.jeffding05.mypmtool.repositories;
+
+import com.jeffding05.mypmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
