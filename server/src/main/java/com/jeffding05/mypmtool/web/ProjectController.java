5ea2a70876919bac398f417696497bd047200c35
jding05 <jeffding05@gmail.com>
2019-11-08 16:32:54 -0800

    add @Valid annotation on createNewProject to change Http.Status from 500 to 400

--- server/src/main/java/com/jeffding05/mypmtool/web/ProjectController.java
+++ server/src/main/java/com/jeffding05/mypmtool/web/ProjectController.java
@@ -6,10 +6,13 @@ import com.jeffding05.mypmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/project")
@@ -20,8 +23,10 @@ public class ProjectController {
 
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+    public ResponseEntity<Project> createNewProject(@Valid @RequestBody Project project) {
+
+
         Project project1 = projectService.saveOrUpdateProject(project);
-        return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+        return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
     }
 }
