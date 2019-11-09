0e5608c17dc90ed156f8913f8230852d0076297a
jding05 <jeffding05@gmail.com>
2019-11-08 17:09:23 -0800

    store all the fieldErrors to HashMap and return the response as a key-value pairs object

--- server/src/main/java/com/jeffding05/mypmtool/web/ProjectController.java
+++ server/src/main/java/com/jeffding05/mypmtool/web/ProjectController.java
@@ -30,7 +30,13 @@ public class ProjectController {
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
 
         if (result.hasErrors()) {
-            return new ResponseEntity<List<FieldError>>(result.getFieldErrors(), HttpStatus.BAD_REQUEST);
+
+            Map<String, String> errorMap = new HashMap<>();
+
+            for (FieldError error : result.getFieldErrors()) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
         }
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
