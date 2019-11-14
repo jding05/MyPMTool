afcdb0f15fc8c1d1c3c9453892f89809656c3f13
jding05 <jeffding05@gmail.com>
2019-11-13 16:55:57 -0800

    add first react component - dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 classname="App-title">Welcome to MyPMTool</h1>
-      </header>
+      <Dashboard />
     </div>
   );
 }
