6ff264e155f0aaf566517d4f6ba68c42ba73acee
jding05 <jeffding05@gmail.com>
2019-11-26 22:25:31 -0800

    get project list from prop and map each project to projectItem

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -11,11 +11,7 @@ class Dashboard extends Component {
   }
 
   render() {
-    const projectObject = {
-      projectName: "ProjectName PROPS",
-      projectIdentifier: "PROP",
-      description: "description from PROPS"
-    };
+    const { projects } = this.props.project;
     return (
       <div className="projects">
         <div className="container">
@@ -26,7 +22,9 @@ class Dashboard extends Component {
               <CreateProjectButton />
               <br />
               <hr />
-              <ProjectItem project={projectObject} />
+              {projects.map(project => (
+                <ProjectItem key={project.id} project={project} />
+              ))}
             </div>
           </div>
         </div>
